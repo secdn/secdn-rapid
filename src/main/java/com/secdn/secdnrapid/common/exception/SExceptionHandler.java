6e134f63c99ee6c63313a6e7e28ef4d927e79b6d
secdn <secdnb@gmail.com>
2019-03-26 18:12:05 +0800

    修改封装方式

--- src/main/java/com/secdn/secdnrapid/common/exception/SExceptionHandler.java
+++ src/main/java/com/secdn/secdnrapid/common/exception/SExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.secdn.secdnrapid.common.exception;
 
-import com.secdn.secdnrapid.common.utils.MessageVoUtil;
-import com.secdn.secdnrapid.common.vo.MessageVo;
+
+import com.secdn.secdnrapid.common.wrapper.WrapMapper;
+import com.secdn.secdnrapid.common.wrapper.Wrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
@@ -18,35 +19,31 @@ public class SExceptionHandler {
 	 * 处理自定义异常
 	 */
 	@ExceptionHandler(SException.class)
-	public MessageVo handleSException(SException e){
-		MessageVo messageVo = MessageVoUtil.getMessageVo();
-		messageVo.setErrorCode(e.getCode());
-		messageVo.setErrorMsg(e.getMessage());
-
-		return messageVo;
+	public Wrapper handleSException(SException e){
+		return WrapMapper.error(e.getCode(), e.getMessage());
 	}
 
 	@ExceptionHandler(NoHandlerFoundException.class)
-	public MessageVo handlerNoFoundException(Exception e) {
+	public Wrapper<Object> handlerNoFoundException(Exception e) {
 		logger.error(e.getMessage(), e);
-		return MessageVoUtil.fail(404, "路径不存在，请检查路径是否正确");
+		return WrapMapper.error(404, "路径不存在，请检查路径是否正确");
 	}
 
 	@ExceptionHandler(DuplicateKeyException.class)
-	public MessageVo handleDuplicateKeyException(DuplicateKeyException e){
+	public Wrapper<Object> handleDuplicateKeyException(DuplicateKeyException e){
 		logger.error(e.getMessage(), e);
-		return MessageVoUtil.fail("数据库中已存在该记录");
+		return WrapMapper.error("数据库中已存在该记录");
 	}
 
 //	@ExceptionHandler(AuthorizationException.class)
 //	public MessageVo handleAuthorizationException(AuthorizationException e){
 //		logger.error(e.getMessage(), e);
-//		return MessageVoUtil.fail("没有权限，请联系管理员授权");
+//		return WrapMapper.error("没有权限，请联系管理员授权");
 //	}
 
 	@ExceptionHandler(Exception.class)
-	public MessageVo handleException(Exception e){
+	public Wrapper<Object> handleException(Exception e){
 		logger.error(e.getMessage(), e);
-		return MessageVoUtil.fail(e.getMessage());
+		return WrapMapper.error(e.getMessage());
 	}
 }
