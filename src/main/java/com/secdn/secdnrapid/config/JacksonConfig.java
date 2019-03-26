5d388a8e3ec348bcdf0f80009ca4486d95658332
secdn <secdnb@gmail.com>
2019-03-26 18:16:50 +0800

    解决前端long类型缺失精度问题

---
+++ src/main/java/com/secdn/secdnrapid/config/JacksonConfig.java
@@ -0,0 +1,27 @@
+package com.secdn.secdnrapid.config;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author secdn
+ * @create 2019-03-26
+ */
+@Configuration
+public class JacksonConfig {
+
+    /**
+     * Jackson全局转化long类型为String，解决前端long类型缺失精度问题
+     *
+     * @return Jackson2ObjectMapperBuilderCustomizer 注入的对象
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        Jackson2ObjectMapperBuilderCustomizer customizer = jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder
+                .serializerByType(Long.class, ToStringSerializer.instance)
+                .serializerByType(Long.TYPE, ToStringSerializer.instance);
+        return customizer;
+    }
+}
