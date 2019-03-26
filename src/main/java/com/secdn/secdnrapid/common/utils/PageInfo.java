5e5faada32c5e208a6ec89e3fe9d9436b37b659f
secdn <secdnb@gmail.com>
2019-03-26 18:13:59 +0800

    修改分页返回参数

--- src/main/java/com/secdn/secdnrapid/common/utils/PageInfo.java
+++ src/main/java/com/secdn/secdnrapid/common/utils/PageInfo.java
@@ -12,7 +12,7 @@ import java.util.List;
  *
  */
 @Data
-public class PageUtils implements Serializable {
+public class PageInfo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	//总记录数
 	private int totalCount;
@@ -23,17 +23,17 @@ public class PageUtils implements Serializable {
 	//当前页数
 	private long currPage;
 	//列表数据
-	private List<?> list;
+	private List<?> records;
 	
 	/**
 	 * 分页
-	 * @param list        列表数据
+	 * @param records        列表数据
 	 * @param totalCount  总记录数
 	 * @param pageSize    每页记录数
 	 * @param currPage    当前页数
 	 */
-	public PageUtils(List<?> list, int totalCount, int pageSize, int currPage) {
-		this.list = list;
+	public PageInfo(List<?> records, int totalCount, int pageSize, int currPage) {
+		this.records = records;
 		this.totalCount = totalCount;
 		this.pageSize = pageSize;
 		this.currPage = currPage;
@@ -43,8 +43,8 @@ public class PageUtils implements Serializable {
 	/**
 	 * 分页
 	 */
-	public PageUtils(IPage<?> page) {
-		this.list = page.getRecords();
+	public PageInfo(IPage<?> page) {
+		this.records = page.getRecords();
 		this.totalCount = (int)page.getTotal();
 		this.pageSize = page.getSize();
 		this.currPage = page.getCurrent();
